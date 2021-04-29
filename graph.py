d1273a73ae80fdae4c88a8708b19c5ae9d10c5ce
Chris <christopher.natoli@sydney.edu.au>
2021-04-29 14:05:20 +1000

    Fix le comments

--- graph.py
+++ graph.py
@@ -16,7 +16,7 @@ All functions to implement are marked with a `TODO` comment.
 Please implement these methods to help the quokkas find their new home!
 """
 
-from typing import List
+from typing import List, Union
 
 from vertex import Vertex
 
@@ -31,27 +31,34 @@ class QuokkaMaze:
     and their final destination.
 
     We _will_ be performing some minor adversarial testing this time, so make
-    sure you're performing checks!
+    sure you're performing checks and ensuring that the graph is a valid simple
+    graph!
 
     ===== Functions =====
 
         * block_edge(u, v) - removes the edge between vertex `u` and vertex `v`
         * fix_edge(u, v) - fixes the edge between vertex `u` and `v`. or adds an
             edge if non-existent
-        * find_path(s, t, k) - find a path from veretx `s` to vertex `t` such
-            that from any location with food along this path we reach the
-            next location with food in at most `k` steps
+        * find_path(s, t, k) - find a SIMPLE path from veretx `s` to vertex `t`
+            such that from any location with food along this simple path we
+            reach the next location with food in at most `k` steps
         * exists_path_with_extra_food(s, t, k, x) - returns whether it’s
-            possible for the quokkas to make it from s to t along a path where
-            from any location with food we reach the next location with food in
-            at most k steps, by placing food at at most x new locations
+            possible for the quokkas to make it from s to t along a simple path
+            where from any location with food we reach the next location with
+            food in at most k steps, by placing food at at most x new locations
 
     ===== Notes ======
 
     * We _will_ be adversarially testing, so make sure you check your params!
     * The ordering of vertices in the `vertex.edges` does not matter.
+    * You MUST check that `k>=0` and `x>=0` for the respective functions
+        * find_path (k must be greater than or equal to 0)
+        * exists_path_with_extra_food (k and x must be greater than or equal to
+            0)
     * This is an undirected graph, so you don't need to worry about the
         direction of traversing during your path finding.
+    * This is a SIMPLE GRAPH, your functions should ensure that it stays that
+        way.
     * All vertices in the graph SHOULD BE UNIQUE! IT SHOULD NOT BE POSSIBLE
         TO ADD DUPLICATE VERTICES! (i.e the same vertex instance)
     """
@@ -71,12 +78,12 @@ class QuokkaMaze:
         :return true if the vertex was correctly added, else false
         """
         # TODO implement me, please?
-        return False
+        pass
 
     def fix_edge(self, u: Vertex, v: Vertex) -> bool:
         """
         Fixes the edge between two vertices, u and v.
-        If an edge already exists, then this operation should fail.
+        If an edge already exists, then this operation should return False.
 
         :param u - A vertex
         :param v - Another vertex
@@ -84,7 +91,7 @@ class QuokkaMaze:
         """
 
         # TODO implement me please.
-        return False
+        pass
 
     def block_edge(self, u: Vertex, v: Vertex) -> bool:
         """
@@ -98,27 +105,46 @@ class QuokkaMaze:
         """
 
         # TODO implement me, please!
-        return False
+        pass
 
-    def find_path(self, s: Vertex, t: Vertex, k: int) -> List[Vertex]:
+    def find_path(
+            self,
+            s: Vertex,
+            t: Vertex,
+            k: int
+    ) -> Union[List[Vertex], None]:
         """
-        find_path returns a path between `s` and `t` such that from any
-        location with food along this path we reach the next location with
-        food in at most `k` steps
+        find_path returns a SIMPLE path between `s` and `t` such that from any
+        location with food along this path we reach the next location with food
+        in at most `k` steps
 
         :param s - The start vertex for the quokka colony
         :param t - The destination for the quokka colony
         :param k - The maximum number of hops between locations with food, so
         that the colony can survive!
         :returns
-            * The list of vertices to form the path from `s` to `t` satisfying the conditions.
+            * The list of vertices to form the simple path from `s` to `t`
+            satisfying the conditions.
             OR
-            * None if no path exists that can satisfy the conditions, or is
-            invalid.
+            * None if no simple path exists that can satisfy the conditions, or
+            is invalid.
+
+        Example:
+        (* means the vertex has food)
+                    *       *
+            A---B---C---D---E
+
+            1/ find_path(s=A, t=E, k=2) -> returns: [A, B, C, D, E]
+
+            2/ find_path(s=A, t=E, k=1) -> returns: None
+            (because there isn't enough food!)
+
+            3/ find_path(s=A, t=C, k=4) -> returns: [A, B, C]
+
         """
 
         # TODO implement me please
-        return None
+        pass
 
     def exists_path_with_extra_food(
         self,
@@ -128,11 +154,35 @@ class QuokkaMaze:
         x: int
     ) -> bool:
         """
-        Determines whether it is possible for the quokkas to make it from s
-        to t along a path where from any location with food we reach the next
-        location with food in at most k steps, by placing food at at most x
-        new locations
-        """
-        # TODO implement me please
+        Determines whether it is possible for the quokkas to make it from s to
+        t along a SIMPLE path where from any location with food we reach the
+        next location with food in at most k steps, by placing food at at most
+        x new locations.
 
-        return False
+        :param s - The start vertex for the quokka colony
+        :param t - The destination for the quokka colony
+        :param k - The maximum number of hops between locations with food, so
+        that the colony can survive!
+        :param x - The number of extra foods to add.
+        :returns
+            * True if with x added food we can complete the simple path
+            * False otherwise.
+
+        Example:
+        (* means the vertex has food)
+                            *
+            A---B---C---D---E
+
+            1/ exists_with_extra_food(A, E, 2, 0) -> returns: False
+                (because we can't get from A to E with k=2 and 0 extra food)
+
+            2/ exists_with_extra_food(A, E, 2, 1) -> returns: True
+                (Yes, if we put food on `C` then we can get to E with k=2)
+
+            3/ exists_with_extra_food(A, E, 1, 6) -> returns: True
+                (Yes, if we put food on `B`, `C`, `D` then we reach E!)
+
+        """
+
+        # TODO implement me please
+        pass
--- vertex.py
+++ vertex.py
@@ -6,6 +6,7 @@ Represents a vertex in the maze, holds the information about other
 connected vertices, and checks whether this vertex has food.
 """
 
+
 class Vertex:
     """
     Vertex represents a location in the quokka's quest to find food.
@@ -33,16 +34,18 @@ class Vertex:
 
     def add_edge(self, v: 'Vertex') -> None:
         """
-        Add an edge between this vertex and vertex 'v'. 
+        Add an edge between this vertex and vertex 'v'.
 
         :param v - The vertex to add an edge between.
         """
         # TODO implement me please!
+        pass
 
     def rm_edge(self, v: 'Vertex') -> None:
         """
-        Removes the edge between this vertex and 'v'. 
+        Removes the edge between this vertex and 'v'.
 
         :param v - The vertex to remove from edges.
         """
         # TODO implement me please!
+        pass
